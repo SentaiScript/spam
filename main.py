df784024af0d637d38741fd27bc27e166adef3f0
SentaiScript <129988959+SentaiScript@users.noreply.github.com>
2026-01-16 15:36:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,1 @@
-import asyncio
-from telethon import TelegramClient
-from telethon.sessions import StringSession
 
-API_ID = 16536645
-API_HASH = "5b23fe1fbbd9f742a182fe8c5f75cbff"
-PHONE = "+447771879860"
-
-async def main():
-    client = TelegramClient(StringSession(), API_ID, API_HASH)
-    await client.start(PHONE)
-    session = client.session.save()
-    print("SESSION:", session)
-    await client.disconnect()
-
-asyncio.run(main())
